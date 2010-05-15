ab62f18c14556e9efb588fade6cef0c87c52e495
ZC Miao <hellwolf.misty@gmail.com>
2010-05-15 17:45:03 +0100

    remove cvs keywards

--- fuse-convmvfs/src/convmvfs.cpp
+++ fuse-convmvfs/src/convmvfs.cpp
@@ -1,10 +1,8 @@
-/* (C) 2006 ZC Miao <hellwolf.misty@gmail.com>
+/* (C) 2006-2010 ZC Miao <hellwolf.misty@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
- *
- * $Id: convmvfs.cpp,v 1.3 2007-06-01 02:25:49 hellwolfmisty Exp $
  *
  */
 
